f9c8b0d9787c89315ad7d62abd0200eae797b36e
Tienta <tathuytien98@gmail.com>
2017-02-20 19:25:21 +0700

    Update Turtle 1.py

--- Turtle 1.py
+++ Turtle 1.py
@@ -2,7 +2,8 @@ from turtle import *
 colors = ['red', 'blue', 'brown', 'yellow', 'grey']
 for i in range (3, 8):
     for j in range (i):
-        color(colors[i-3])
+        trick = (i-3)%len(colors)
+        color(colors(trick))
         fd(100)
         lt(360/i)
         
